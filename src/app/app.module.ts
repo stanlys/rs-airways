62f42a922a145dab7f4ac8c611d5f023b3185138
Alexei Arushanov <3k4ffb8dj@mozmail.com>
2023-04-14 09:44:58 +0300

    build: init angular app
    add eslint, material, redux

---
+++ src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StoreModule } from '@ngrx/store';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, StoreModule.forRoot({}, {})],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
