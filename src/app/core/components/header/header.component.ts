6a33b8ac03db3b54d364deef8ef5e851a9929d4c
Alexei Arushanov <3k4ffb8dj@mozmail.com>
2023-04-23 10:55:49 +0300

    style: add apply main page theme for header
    the theme is applied dynamically in header component on navigation, by adding the "main-page" class to the body element

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -6,6 +8,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
+  constructor(router: Router) {
+    router.events.pipe(filter((e) => e instanceof NavigationEnd)).subscribe((e) => {
+      if (e instanceof NavigationEnd && e.urlAfterRedirects.includes('main')) {
+        document.body.classList.add('main-page');
+      } else {
+        document.body.classList.remove('main-page');
+      }
+    });
+  }
+
   // TODO: implement auth
   // public showAuth(): void {}
 }
