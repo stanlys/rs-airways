3b56168d6f2b8632f1842fa5591fa0d734864b0c
Stanlys <3995782+stanlys@users.noreply.github.com>
2023-04-24 19:57:36 +0300

    refactor: correct forlder strucure

--- src/app/shared/material/material.module.ts
+++ src/app/shared/material/material.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [],
   imports: [CommonModule],
-  exports: [MatButtonModule, MatCardModule],
+  exports: [MatButtonModule, MatCardModule, MatFormFieldModule, MatInputModule],
 })
 export class MaterialModule {}
