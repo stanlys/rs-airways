044ca093d38fa93948feb0aa9de9ea8623b5b8b8
Stanlys <3995782+stanlys@users.noreply.github.com>
2023-04-25 22:54:08 +0300

    feat: add footer to cart

--- src/app/shared/material/material.module.ts
+++ src/app/shared/material/material.module.ts
@@ -6,10 +6,23 @@ import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [],
   imports: [CommonModule, MatCardModule, MatTableModule],
-  exports: [MatCardModule, MatTableModule, MatDividerModule, MatIconModule, MatMenuModule, MatCheckboxModule],
+  exports: [
+    MatCardModule,
+    MatTableModule,
+    MatDividerModule,
+    MatIconModule,
+    MatMenuModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+  ],
 })
 export class MaterialModule {}
