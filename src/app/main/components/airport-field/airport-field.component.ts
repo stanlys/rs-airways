d4f47c44e8a72138738918ffbf98a90092bc4ffe
AnnaFeona <glavatskikh.anna@gmail.com>
2023-04-28 01:15:45 +0300

    refactor: replace form-fiels elements to a separate components

--- src/app/main/components/airport-field/airport-field.component.ts
+++ src/app/main/components/airport-field/airport-field.component.ts
@@ -1,10 +1,46 @@
+/* eslint-disable no-underscore-dangle */
 import { Component } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { Observable, startWith, map } from 'rxjs';
+import { Airport } from '../../model/main.interfaces';
+import { AIRPORTS } from '../../model/mock-list.model';
 
 @Component({
   selector: 'app-airport-field',
   templateUrl: './airport-field.component.html',
-  styleUrls: ['./airport-field.component.scss']
+  styleUrls: ['./airport-field.component.scss'],
 })
 export class AirportFieldComponent {
+  public myControl: FormControl;
 
+  public filteredOptions: Observable<Airport[]> | undefined;
+
+  public options = AIRPORTS;
+
+  constructor() {
+    this.myControl = new FormControl<string | Airport>('', Validators.required);
+  }
+
+  public ngOnInit(): void {
+    this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map((value) => {
+        if (!value) {
+          return this.options;
+        }
+        return this._filter(value as string);
+      })
+    );
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  public showAirport(airport: Airport): string {
+    return airport && airport.city ? `${airport.city} ${airport.name ? airport.name : ''}, ${airport.code}` : '';
+  }
+
+  private _filter(value: string): Airport[] {
+    const filterValue = value.toLowerCase();
+
+    return this.options.filter((option) => option.city.toLowerCase().includes(filterValue));
+  }
 }
--- src/app/main/components/main-form/main-form.component.ts
+++ src/app/main/components/main-form/main-form.component.ts
@@ -10,40 +10,10 @@ import { AIRPORTS } from '../../model/mock-list.model';
   templateUrl: './main-form.component.html',
   styleUrls: ['./main-form.component.scss'],
 })
-export class MainFormComponent implements OnInit {
+export class MainFormComponent {
   public searchForm: FormBuilder;
-
-  public myControl: FormControl;
-
-  public filteredOptions: Observable<Airport[]> | undefined;
-
-  public options = AIRPORTS;
 
   constructor() {
     this.searchForm = new FormBuilder();
-    this.myControl = new FormControl<string | Airport>('', Validators.required);
-  }
-
-  public ngOnInit(): void {
-    this.filteredOptions = this.myControl.valueChanges.pipe(
-      startWith(''),
-      map((value) => {
-        if (!value) {
-          return this.options;
-        }
-        return this._filter(value as string);
-      })
-    );
-  }
-
-  // eslint-disable-next-line class-methods-use-this
-  public showAirport(airport: Airport): string {
-    return airport && airport.city ? `${airport.city} ${airport.name ? airport.name : ''}, ${airport.code}` : '';
-  }
-
-  private _filter(value: string): Airport[] {
-    const filterValue = value.toLowerCase();
-
-    return this.options.filter((option) => option.city.toLowerCase().includes(filterValue));
   }
 }
