7bbcd05925d93981085bf81125ef38aa63fa41e3
Alexei Arushanov <3k4ffb8dj@mozmail.com>
2023-04-27 21:02:14 +0300

    style: adjust date-format container and panel appearance

--- src/app/core/components/header/date-format-selector/date-format-selector.component.ts
+++ src/app/core/components/header/date-format-selector/date-format-selector.component.ts
@@ -7,11 +7,11 @@ import { STORAGE_KEY_PREFIX } from '../../../../shared/constants';
   styleUrls: ['./date-format-selector.component.scss'],
 })
 export class DateFormatSelectorComponent {
-  public readonly dateFormats = ['MM/DD/YYYY', 'DD/MM/YYYY', 'YYYY/DD/MM', 'YYYY/MM/DD'];
+  public readonly dateOpts = ['MM/DD/YYYY', 'DD/MM/YYYY', 'YYYY/DD/MM', 'YYYY/MM/DD'];
 
   public dateFormatItemKey = `${STORAGE_KEY_PREFIX}-dateFormat`;
 
-  public selectedDateFormat = localStorage.getItem(this.dateFormatItemKey) || this.dateFormats[0];
+  public selectedDateFormat = localStorage.getItem(this.dateFormatItemKey) || this.dateOpts[0];
 
   public selectDateFormat(v: string): void {
     this.selectedDateFormat = v;
