c50f37e1397032486149d762f3b805495baaffb2
Alexei Arushanov <3k4ffb8dj@mozmail.com>
2023-04-28 03:56:30 +0300

    improvement: add citizenship select field

--- src/app/core/components/auth/signup-tab/signup-tab.component.ts
+++ src/app/core/components/auth/signup-tab/signup-tab.component.ts
@@ -1,8 +1,8 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { getNames } from 'country-list';
 import { Subject, delay, of } from 'rxjs';
 
-import { MatButtonToggleGroup } from '@angular/material/button-toggle';
 import { passwordStrengthValidator } from '../../../directives/password-strength-validator.directive';
 import { AuthService } from '../../../services/auth.service';
 
@@ -14,6 +14,7 @@ interface SignupForm {
   birthDate: FormControl<Date | null>;
   confirm: FormControl<boolean | null>;
   gender: FormControl<string | null>;
+  citizenship: FormControl<string | null>;
 }
 
 @Component({
@@ -29,6 +30,8 @@ export class SignupTabComponent {
   @Output() public closeModal = new EventEmitter<void>();
 
   public maxDate = new Date();
+
+  public countryNames = getNames();
 
   constructor(fb: FormBuilder, private authService: AuthService) {
     this.form = fb.group<SignupForm>({
@@ -49,6 +52,7 @@ export class SignupTabComponent {
       birthDate: new FormControl(null, [Validators.required]),
       confirm: new FormControl(null, [Validators.required]),
       gender: new FormControl('', [Validators.required]),
+      citizenship: new FormControl('', [Validators.required]),
     });
   }
 
@@ -57,6 +61,8 @@ export class SignupTabComponent {
   }
 
   public onSubmit(): void {
+    console.log(this.form.value);
+
     this.authService.signup();
 
     this.isLoading$.next(true);
