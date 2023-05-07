87f7742db80c95bbb98480122fd2c7f9a92595cc
Stanlys <stanlys@yandex.ru>
2023-05-07 17:26:02 +0300

    feat: create information about passenger

--- src/app/booking/booking.module.ts
+++ src/app/booking/booking.module.ts
@@ -5,9 +5,10 @@ import { BookingRoutingModule } from './booking-routing.module';
 import { BookingPageComponent } from './pages/booking-page/booking-page.component';
 import { SharedModule } from '../shared/shared.module';
 import { SummaryPageComponent } from './pages/summary-page/summary-page.component';
+import { InformationAboutPassengerComponent } from './components/information-about-passenger/information-about-passenger.component';
 
 @NgModule({
-  declarations: [BookingPageComponent, SummaryPageComponent],
+  declarations: [BookingPageComponent, SummaryPageComponent, InformationAboutPassengerComponent],
   imports: [CommonModule, BookingRoutingModule, SharedModule],
 })
 export class BookingModule {}
---
+++ src/app/booking/components/information-about-passenger/information-about-passenger.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-information-about-passenger',
+  templateUrl: './information-about-passenger.component.html',
+  styleUrls: ['./information-about-passenger.component.scss'],
+})
+export class InformationAboutPassengerComponent {}
