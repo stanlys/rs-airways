62fb20de6f09da6ff0c03afd6d0c7ddb100a428e
Stanlys <stanlys@yandex.ru>
2023-05-07 16:45:39 +0300

    feat: start summary page

--- src/app/booking/booking.module.ts
+++ src/app/booking/booking.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { BookingRoutingModule } from './booking-routing.module';
 import { BookingPageComponent } from './pages/booking-page/booking-page.component';
 import { SharedModule } from '../shared/shared.module';
+import { SummaryPageComponent } from './pages/summary-page/summary-page.component';
 
 @NgModule({
-  declarations: [BookingPageComponent],
+  declarations: [BookingPageComponent, SummaryPageComponent],
   imports: [CommonModule, BookingRoutingModule, SharedModule],
 })
 export class BookingModule {}
