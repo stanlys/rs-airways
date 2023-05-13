9d6dbc0fee8cf9a42e6992ed5a71bc1ef4d48415
Alexei Arushanov <3k4ffb8dj@mozmail.com>
2023-05-14 02:35:53 +0300

    refactor: break flights into flights-calendar and flights-destination

--- src/app/booking/components/flights-calendar-destination/flights-calendar-destination.component.ts
+++ src/app/booking/components/flights-calendar-destination/flights-calendar-destination.component.ts
@@ -1,8 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+import { Flight } from '../../../shared/models/flight-search.interfaces';
 
 @Component({
   selector: 'app-flights-calendar-destination',
   templateUrl: './flights-calendar-destination.component.html',
   styleUrls: ['./flights-calendar-destination.component.scss'],
 })
-export class FlightsCalendarDestinationComponent {}
+export class FlightsCalendarDestinationComponent {
+  @Input() public flight!: Flight;
+
+  @Input() public odd!: boolean;
+}
--- src/app/booking/components/flights-calendar/flights-calendar.component.ts
+++ src/app/booking/components/flights-calendar/flights-calendar.component.ts
@@ -1,8 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import dayjs from 'dayjs';
+
+import { Flight } from '../../../shared/models/flight-search.interfaces';
 
 @Component({
   selector: 'app-flights-calendar',
   templateUrl: './flights-calendar.component.html',
   styleUrls: ['./flights-calendar.component.scss'],
 })
-export class FlightsCalendarComponent {}
+export class FlightsCalendarComponent implements OnInit {
+  @Input() public flight!: Flight;
+
+  @Output() public selectedFlightChange = new EventEmitter<Flight>();
+
+  public selectedFlightNumber?: string;
+
+  public dates: Date[] = [];
+
+  public flights: Flight[] = [];
+
+  public ngOnInit(): void {
+    this.dates = this.generateStartingDates();
+    const { otherFlights, ...flight } = this.flight;
+    this.selectedFlightNumber = flight.flightNumber;
+    this.flights = [...Object.values(otherFlights || {}), flight];
+  }
+
+  public decrementDate(): void {
+    const firstDate = dayjs(this.dates[0]).subtract(1, 'day').toDate();
+    this.dates = [firstDate, ...this.dates.slice(0, -1)];
+  }
+
+  public incrementDate(): void {
+    const lastDate = dayjs(this.dates[this.dates.length - 1])
+      .add(1, 'day')
+      .toDate();
+    this.dates = [...this.dates.slice(1), lastDate];
+  }
+
+  public selectDate(v: string): void {
+    const flight = this.flights.find((f) => f.flightNumber === v);
+    if (flight) {
+      this.selectedFlightChange.emit(flight);
+      this.selectedFlightNumber = flight.flightNumber;
+    }
+  }
+
+  private generateStartingDates(): Date[] {
+    return Array.from({ length: 5 }, (_, i) => {
+      return dayjs(this.flight.takeoffDate).subtract(2, 'day').add(i, 'day').toDate();
+    });
+  }
+}
