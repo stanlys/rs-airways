4e66f216a79d1bf614db9875632503948c8da19e
AnnaFeona <glavatskikh.anna@gmail.com>
2023-05-14 13:04:32 +0300

    style: customize mat-slider-toggle element

--- src/app/booking/components/booking-passenger-card/booking-passenger-card.component.ts
+++ src/app/booking/components/booking-passenger-card/booking-passenger-card.component.ts
@@ -1,14 +1,18 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewChild, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-booking-passenger-card',
   templateUrl: './booking-passenger-card.component.html',
   styleUrls: ['./booking-passenger-card.component.scss'],
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  encapsulation: ViewEncapsulation.None,
 })
 export class BookingPassengerCardComponent {
   @Input() public index = 1;
 
   @Input() public title = 'Adult';
+
+  public tooltip = `Add the passenger's name as it is written on their documents (passport or ID). Do not use any accents or special characters. Do not use a nickname.`;
 
   // public ngOnInit(): void {}
 }
