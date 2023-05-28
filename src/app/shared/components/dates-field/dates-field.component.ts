a8b331c08edea41e7b30ca81593b9a917f80fb7d
Alexei Arushanov <3k4ffb8dj@mozmail.com>
2023-05-29 02:46:05 +0300

    feat: use custom format as MAT_DATE_FORMATS value

--- src/app/shared/components/dates-field/dates-field.component.ts
+++ src/app/shared/components/dates-field/dates-field.component.ts
@@ -1,18 +1,42 @@
 import { ChangeDetectorRef, Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { FormControl, FormGroup, FormGroupDirective, Validators } from '@angular/forms';
+import { MAT_MOMENT_DATE_ADAPTER_OPTIONS, MomentDateAdapter } from '@angular/material-moment-adapter';
+import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
 import { Subject, takeUntil } from 'rxjs';
+
+import { DateFormatService } from '../../services/date-format.service';
+
+export const MY_FORMATS = {
+  parse: {
+    dateInput: 'LL',
+  },
+  display: {
+    dateInput: 'DD-MM-YYYY',
+    monthYearLabel: 'YYYY',
+    dateA11yLabel: 'LL',
+    monthYearA11yLabel: 'YYYY',
+  },
+};
 
 @Component({
   selector: 'app-dates-field',
   templateUrl: './dates-field.component.html',
   styleUrls: ['./dates-field.component.scss'],
+  providers: [
+    {
+      provide: DateAdapter,
+      useClass: MomentDateAdapter,
+      deps: [MAT_DATE_LOCALE, MAT_MOMENT_DATE_ADAPTER_OPTIONS],
+    },
+    { provide: MAT_DATE_FORMATS, useValue: MY_FORMATS },
+  ],
 })
 export class DatesFieldComponent implements OnInit, OnDestroy {
   @Input() public name!: string;
 
   @Input() public showFormatHint = true;
 
-  public dateFormatHint = 'MM/DD/YYYY - MM/DD/YYYY';
+  public dateFormatHint = this.dateFormatService.getDateFormat();
 
   public oneWay?: boolean;
 
@@ -28,7 +52,11 @@ export class DatesFieldComponent implements OnInit, OnDestroy {
 
   public destroy$ = new Subject<void>();
 
-  constructor(public parentForm: FormGroupDirective, private cd: ChangeDetectorRef) {}
+  constructor(
+    public parentForm: FormGroupDirective,
+    private cd: ChangeDetectorRef,
+    private dateFormatService: DateFormatService
+  ) {}
 
   public ngOnInit(): void {
     this.datesForm = this.parentForm.control.get(this.name) as FormGroup;
@@ -36,18 +64,24 @@ export class DatesFieldComponent implements OnInit, OnDestroy {
     this.takeoffDate = this.datesForm.get('takeoffDate') as FormControl<Date>;
     this.landingDate = this.datesForm.get('landingDate') as FormControl<Date>;
 
-    this.oneWay = this.parentForm.control.get('oneWay')?.value as boolean;
+    const oneWay = !!this.parentForm.control.get('oneWay')?.value;
+    this.oneWay = oneWay;
 
     this.parentForm.control
       .get('oneWay')
       ?.valueChanges.pipe(takeUntil(this.destroy$))
-      .subscribe((v) => {
-        this.oneWay = v as boolean;
+      .subscribe((v: boolean) => {
+        this.oneWay = v;
 
         this.updateValidators(this.oneWay);
         this.updateHint(this.oneWay);
         this.cd.detectChanges();
       });
+
+    this.dateFormatService.dateFormat$.pipe(takeUntil(this.destroy$)).subscribe((v) => {
+      this.updateHint(oneWay, v);
+      MY_FORMATS.display.dateInput = v.replace('/', '-');
+    });
   }
 
   public ngOnDestroy(): void {
@@ -55,8 +89,8 @@ export class DatesFieldComponent implements OnInit, OnDestroy {
     this.destroy$.complete();
   }
 
-  private updateHint(flag: boolean): void {
-    this.dateFormatHint = flag ? 'MM/DD/YYYY' : 'MM/DD/YYYY - MM/DD/YYYY';
+  private updateHint(flag: boolean, v: string = this.dateFormatService.getDateFormat()): void {
+    this.dateFormatHint = flag ? v : `${v} - ${v}`;
   }
 
   private updateValidators(flag: boolean): void {
--- src/app/shared/material/material.module.ts
+++ src/app/shared/material/material.module.ts
@@ -1,28 +1,28 @@
 import { NgModule } from '@angular/core';
-import { MatCardModule } from '@angular/material/card';
-import { MatRadioModule } from '@angular/material/radio';
-import { MatInputModule } from '@angular/material/input';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatBadgeModule } from '@angular/material/badge';
 import { MatButtonModule } from '@angular/material/button';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
-import { MatIconModule } from '@angular/material/icon';
-import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatCardModule } from '@angular/material/card';
 import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatCommonModule, MatNativeDateModule } from '@angular/material/core';
+import { MatCommonModule, MatNativeDateModule, NativeDateModule } from '@angular/material/core';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatSelectModule } from '@angular/material/select';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatSortModule } from '@angular/material/sort';
 import { MatStepperModule } from '@angular/material/stepper';
+import { MatTableModule } from '@angular/material/table';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatTableModule } from '@angular/material/table';
-import { MatSortModule } from '@angular/material/sort';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatBadgeModule } from '@angular/material/badge';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
@@ -53,6 +53,7 @@ import { MatTooltipModule } from '@angular/material/tooltip';
     MatBadgeModule,
     MatSlideToggleModule,
     MatTooltipModule,
+    NativeDateModule,
   ],
 })
 export class MaterialModule {}
